e4e7e9c6617b6d3caf1580361eeba10cd2bf3b64
zhenglianfu <15067134439@163.com>
2014-08-14 20:57:21 +0800

    init handler Excel

--- src/main/java/com/lbyt/client/service/ClientService.java
+++ src/main/java/com/lbyt/client/service/ClientService.java
@@ -1,5 +1,22 @@
 package com.lbyt.client.service;
 
-public class ClientService {
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
+import com.lbyt.client.bean.JsonBean;
+import com.lbyt.client.util.ExcelUtil;
+import com.lbyt.client.util.ExcelUtil.Sheet;
+
+public class ClientService {
+	
+	public JsonBean importExcel(){
+		JsonBean jsonBean = new JsonBean();
+		jsonBean.setSuccess(false);
+		return jsonBean;
+	} 
+	
+	private void parseExcel(File file) throws IOException{
+		List<Sheet> sheets = ExcelUtil.parseExcel(file);
+	}
 }
